d3533b414422310607e45fe69e5e3edfeba9ca04
Mukola-P <proxorov.mukola@gmail.com>
2023-01-14 20:00:37 +0200

    task_1-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import { Profile } from "components/Profile/Profile";
+import user from "basis/user.json";
+
+import { Statistics } from "components/Statistics/Statistics";
+import data from "basis/data.json";
+
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+     <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      {/* <FriendList friends={friends} />
+      <TransactionHistory items={transactions} /> */}
     </div>
   );
-};
+}
